2ba49bdde4c18850913caee844557d8645cf2625
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-28 22:18:26 +0000

    sending html and json requests/responses

--- express_server.js
+++ express_server.js
@@ -15,6 +15,12 @@ app.get("/", (request, response) => {
 app.get("/url.json", (request, response) => {
   response.json(urlDatabase);
 })
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body> Hello <b> World </b></body></html>\n");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
