51f86fab24368891bf8fd3ab0bf5f61e95fd65ac
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-29 01:52:40 +0000

    added views and partials

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+app.set("view engine", "ejs");    //tells exprees app to use EJS as it's templating engine
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthoutlabs.ca",
@@ -19,6 +21,15 @@ app.get("/url.json", (request, response) => {
 app.get("/hello", (request, response) => {
   response.send("<html><body> Hello <b> World </b></body></html>\n");
 })
+app.get("/urls", (request, response) => {
+  const templateVars = { urls: urlDatabase } 
+  response.render("urls_index", templateVars);
+})
+
+app.get('/urls/:shortURL', (request, response) => {
+  const templateVars = { shortURL: request.params.shortURL, longURL: `/urls/${request.params.shortURL}`};
+  response.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
