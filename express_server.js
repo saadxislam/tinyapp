d1b10d4a946356f80780b7f504b7382272643a57
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-29 02:59:55 +0000

    added POST, bodyparser, how to take a form, send back long form URL

--- express_server.js
+++ express_server.js
@@ -2,13 +2,32 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");    //tells exprees app to use EJS as it's templating engine
 
-
+function generateRandomString() {
+  Math.random()*10000
+  
+}
+console.log(generateRandomString());
 const urlDatabase = {
   "b2xVn2": "http://www.lighthoutlabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+/*
+After our browser renders our new URL form, the user populates the form with a longURL and presses submit.
+Our browser sends a POST request to our server.
+Our server logs the request body to the console, then responds with 200 OK.
+Our browser renders the "Ok" message.
+*/
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});                               // respond with "ok"
 
 app.get("/", (request, response) => {
   response.send("Hello!")
